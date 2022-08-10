a2ce23426ef4e3c0f80275922b92a6590a321852
Cengiz Han <sonsair11@gmail.com>
2022-08-10 15:36:15 +0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,74 @@
+const scoreDisplay = document.getElementById('score-display');
+const questionDisplay = document.getElementById('question-display')
+
+const questions =[
+{
+    quiz: ['value','estimate','evaluate'],
+    options : ['jury','assess'],
+    correct : 2
+},
+{
+    quiz: ['value','estimate','evaluate'],
+    options : ['jury','assess'],
+    correct : 2
+},
+{
+    quiz: ['value','estimate','evaluate'],
+    options : ['jury','assess'],
+    correct : 2
+},
+{
+    quiz: ['value','estimate','evaluate'],
+    options : ['jury','assess'],
+    correct : 1
+},
+{
+    quiz: ['value','estimate','evaluate'],
+    options : ['jury','assess'],
+    correct : 2
+},
+]
+let score = 0
+scoreDisplay.textContent = score
+
+function populateQuestions(){
+    questions.forEach(question => {
+        const questionBox = document.createElement('div')
+        questionBox.classList.add('question-box')
+
+        const logoDisplay = document.createElement('h1')
+        logoDisplay.textContent = "~"
+        questionBox.append(logoDisplay)
+
+        question.quiz.forEach(tip=>{
+            const tipText=document.createElement("p")
+            tipText.textContent=tip
+            questionBox.append(tipText)
+        })
+
+       const questionButtons = document.createElement('div')
+       questionButtons.classList.add('question-buttons')
+       questionBox.append(questionButtons)
+
+       question.options.forEach((option,optionIndex) => {
+        const questionButton = document.createElement('button')
+        questionButton.classList.add('question-button')
+        questionButton.textContent = option
+
+        questionButton.addEventListener('click',() => checkAnswer(option,optionIndex +1, question.correct))
+
+        questionButtons.append(questionButton)
+       })
+
+
+
+        questionDisplay.append(questionBox)
+    })
+}
+populateQuestions();
+
+
+function checkAnswer(option,optionIndex){
+    console.log('option',option)
+    console.log('optionIndex',optionIndex)
+}
